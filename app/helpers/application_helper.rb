ce883bbc605d6719ce4a7aaf3f439d7b206259e3
morashid92 <rashid_92@hotmail.co.uk>
2015-12-14 22:01:26 +0000

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+	# Returns the full title on a per-page basis.
+	   
+	def full_title(page_title = '') #argument assignment
+		base_title = "Ruby on Rails Tutorial Sample App" #variable assignment
+		if page_title.empty?   #control flow, #boolean tests
+			base_title 
+		else #control flow
+			page_title + " | " + base_title #string concatenation
+		end
+	end
 end
+
+
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
-    get :home
-    assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
-  end
+  test "should get home" do                                         # body of test based on do is a block 
+    get :home                                                       
+    assert_response :success                                        # test method takes string as argument and 
+    assert_select "title", "Ruby on Rails Tutorial Sample App"      # a block and executes the body of the block   
+  end                                                               # as part of running test suite.
 
   test "should get help" do
     get :help
